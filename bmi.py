64225fa1d7284c2c27f43222c76b1ec8d6cc613e
Zhou Yining <ZHOU.24@ichat.sp.edu.sg>
2025-04-28 11:54:14 +0800

    bmi calculator

---
+++ bmi.py
@@ -0,0 +1,23 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight/(height*height)
+    print ("Your BMI is " + str(bmi))
+    return bmi
+
+def check_bmi(bmi_value):
+    if (bmi_value > 25.0):
+        print("you are over weight")
+    if (bmi_value >= 18.5 and bmi_value <= 25.0):
+        print("you are very healthy")
+    if (bmi_value < 18.5):
+        print("you are under weight")
+
+def main():
+    h =float(input("Enter your height : "))
+    w =float(input("Enter your weight : "))
+    bmi_value = calculate_bmi(h, w)
+    check_bmi(bmi_value)
+
+if __name__ =="__main__":
+    main()
